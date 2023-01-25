e7c439b021c83f7a74e16dcd3aeb9ef31562405a
ajlarson24 <123599236+ajlarson24@users.noreply.github.com>
2023-01-25 17:07:09 -0600

    updated look

--- unit-1-master/Chapter01/js/main.js
+++ unit-1-master/Chapter01/js/main.js
@@ -1,5 +1,5 @@
 // Add all scripts to the JS folder
 var mydiv = document.getElementById("mydiv");
             mydiv.addEventListener("click", function(){
-                alert("Hello World!");
+                alert("Alex says hi! This is my first webpage");
             });
