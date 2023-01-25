737d10fa163d1ab1b11917257925043edff82b48
ajlarson24 <123599236+ajlarson24@users.noreply.github.com>
2023-01-25 17:01:04 -0600

    update text

--- unit-1-master/Chapter01/js/main.js
+++ unit-1-master/Chapter01/js/main.js
@@ -1,1 +1,5 @@
 // Add all scripts to the JS folder
+var mydiv = document.getElementById("mydiv");
+            mydiv.addEventListener("click", function(){
+                alert("Hello World!");
+            });
