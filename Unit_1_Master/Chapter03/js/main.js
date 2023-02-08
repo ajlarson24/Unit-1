8cd87ea1de7f391cba8d3ce3f1f526be05f3f9b4
ajlarson24 <123599236+ajlarson24@users.noreply.github.com>
2023-02-08 17:45:54 -0600

    Activity 4 pt 1

---
+++ Unit_1_Master/Chapter03/js/main.js
@@ -0,0 +1,23 @@
+// Add all scripts to the JS folder
+//Example 3.5...
+function jsAjax(){
+    //define a variable to hold the data
+    var myData;
+
+    //basic fetch
+    fetch('data/MegaCities.geojson')
+        .then(function(response){
+            return response.json();
+        }) 
+        .then(function(response){
+            myData = response;
+
+            //check the data, data is usable here
+            console.log(myData)
+        }) 
+
+    //check the data, data is undefined here
+    console.log(myData)
+};
+
+document.addEventListener('DOMContentLoaded',jsAjax)
