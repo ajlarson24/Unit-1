5fb0944da91706454d885c4aab177ed96cbe9027
ajlarson24 <123599236+ajlarson24@users.noreply.github.com>
2023-02-13 16:54:58 -0600

    Activity 4 Turn-In

--- Unit_1_Master/Chapter03/js/debug_ajax.js
+++ Unit_1_Master/Chapter03/js/debug_ajax.js
@@ -1,6 +1,7 @@
-function debugCallback(response){
+//THIS FUNCTION IS NOT NEEDED, WE CAN CREATED A NESTED FUNCTION WITH THE SAME EFFECT
+/*function debugCallback(response){
 	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(myData))
-};
+};*/
 
 function debugAjax(){
 	
@@ -8,10 +9,18 @@ function debugAjax(){
 	
 	fetch("data/MegaCities.geojson")
 		.then(function(response){
-			debugCallback(response);
+			//this debugCallback is not created, so we can use a different form
+			//debugCallback(response);
+			return response.json();
 		})
+		.then(function(response){
+			myData = response
 
-	document.querySelector("#mydiv").insertAdjacentHTML('beforeend' '<br>GeoJSON data:<br>' + JSON.stringify(myData))
+			document.querySelector("#mydiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:</br>' + JSON.stringify(myData))
+		})
+	//THIS LINE IS MOVED INTO THE CONVERTED CALLBACK FUNCTION ABOVE
+	//document.querySelector("#mydiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:</br>' + JSON.stringify(myData))
 };
 
-document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(myData))
+//THIS LINE IS NOT NEEDED AS THE MYDATA HERE IS UNDEFINED
+//document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(myData))
